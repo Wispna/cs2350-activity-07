f0b31ae073358916989afc2f883def76542a2eb8
Wispna <sundavaris@gmail.com>
2021-10-27 20:59:19 -0600

    second commit

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -4,3 +4,11 @@ require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
 require.context('../stylesheets/', true, /\.(css)$/i)
 
 //TODO - Your ES6 JavaScript code (if any) goes here
+import "bootstrap"
+let number = 1;
+ for (x=0; x<12; x++) {
+    for (y=0; y<12; y++) {
+        document.write(number*x);
+    }
+ }
+document.write('<div class="box">' + 4 + '</div>');
